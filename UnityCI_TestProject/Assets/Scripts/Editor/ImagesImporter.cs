954208910415144fbc70b265707bbeee57666df2
ImInfenix <duncan.sourdin@etudiant.univ-rennes1.fr>
2021-02-01 23:42:03 +0100

    Completed Unity test project

---
+++ UnityCI_TestProject/Assets/Scripts/Editor/ImagesImporter.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class ImagesImporter : AssetPostprocessor
+{
+    void OnPreprocessTexture()
+    {
+        TextureImporter textureImporter = (TextureImporter)assetImporter;
+        textureImporter.textureType = TextureImporterType.Sprite;
+        textureImporter.filterMode = FilterMode.Point;
+        textureImporter.textureCompression = TextureImporterCompression.Uncompressed;
+    }
+}
---
+++ UnityCI_TestProject/Assets/Scripts/ValueIncreaser.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using Text = TMPro.TMP_Text;
+
+public class ValueIncreaser : MonoBehaviour
+{
+    const int maxValue = 9;
+    const int resetValue = 0;
+
+    [SerializeField]
+    private Text leftText;
+    [SerializeField]
+    private Text rightText;
+    [SerializeField]
+    private Text additionResult;
+
+    private int leftValue;
+    private int rightValue;
+
+    private void Awake()
+    {
+        leftValue = resetValue;
+        rightValue = resetValue;
+
+        DisplayValues();
+    }
+
+    public void IncreaseLeft()
+    {
+        leftValue++;
+        ClampValues();
+    }
+
+    public void IncreaseRight()
+    {
+        rightValue++;
+        ClampValues();
+    }
+
+    private void ClampValues()
+    {
+        if (leftValue > maxValue)
+            leftValue = resetValue;
+        if (rightValue > maxValue)
+            rightValue = resetValue;
+
+        DisplayValues();
+    }
+
+    private void DisplayValues()
+    {
+        leftText.text = leftValue.ToString();
+        rightText.text = rightValue.ToString();
+
+        additionResult.text = (leftValue + rightValue).ToString();
+    }
+}
